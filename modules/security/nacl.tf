e93131d767815e4e6f2f6465f34f55a396ef6e0c
DKBIT1792 <dkbit1792@gmail.com>
2024-08-22 12:16:24 +0530

    verified latest application changes reflection and accessibility

--- modules/security/nacl.tf
+++ modules/security/nacl.tf
@@ -11,11 +11,35 @@ resource "aws_network_acl_rule" "public_http_ingress" {
   network_acl_id = aws_network_acl.public_nacl.id
   rule_number = 100
   egress = false
-  protocol = "all"
+  protocol = "tcp"
   cidr_block = "0.0.0.0/0"
+  from_port = 80
+  to_port = 80
   rule_action = "allow"
 }
 
+resource "aws_network_acl_rule" "public_https_ingress" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number = 110
+  egress = false
+  protocol = "tcp"
+  cidr_block = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+  rule_action = "allow"
+}
+
+
+resource "aws_network_acl_rule" "public_tcp_ingress" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number = 120
+  egress = false
+  protocol = "tcp"
+  cidr_block = "0.0.0.0/0"
+  from_port = 1024
+  to_port = 65535
+  rule_action = "allow"
+}
 
 resource "aws_network_acl_rule" "public_nacl_egress" {
     network_acl_id =  aws_network_acl.public_nacl.id
@@ -54,11 +78,22 @@ resource "aws_network_acl_rule" "allow_all_inbound_private" {
   network_acl_id = aws_network_acl.private_nacl.id
   egress = false
   protocol = "tcp"
-  rule_number = 200
-  rule_action = "allow"
+  rule_number = 100
   cidr_block = "0.0.0.0/0"
   from_port = 80
   to_port = 443
+  rule_action = "allow"
+}
+
+resource "aws_network_acl_rule" "allow_tcp_private" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  egress = false
+  protocol = "tcp"
+  rule_number = 110
+  cidr_block = "0.0.0.0/0"
+  from_port = 1024
+  to_port = 65535
+  rule_action = "allow"
 }
 
 resource "aws_network_acl_association" "private_nacl_association" {
--- modules/security/sg.tf
+++ modules/security/sg.tf
@@ -42,6 +42,13 @@ resource "aws_security_group" "ecs_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
