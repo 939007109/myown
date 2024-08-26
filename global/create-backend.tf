2ba70bf1a5d8550d97a0e145d9046f7df4d0ffc0
Alavala Sai Kumar <108181643+939007109@users.noreply.github.com>
2024-08-26 15:58:25 +0530

    Update create-backend.tf

--- global/create-backend.tf
+++ global/create-backend.tf
@@ -1,33 +1,36 @@
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "project-9acts-terraform-state-bucket123"
 
-    versioning {
-        enabled = true
-    }
+  lifecycle {
+    prevent_destroy = true
+  }
 
-    lifecycle {
-        prevent_destroy = true
-    }
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Global"
+  }
+}
 
-    tags = {
-        Name        = "Terraform State Bucket"
-        Environment = "Global"
-    }
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-state-lock"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-    tags = {
-      Name        = "Terraform State Locks Table"
-      Environment = "Global"
-    }
+  tags = {
+    Name        = "Terraform State Locks Table"
+    Environment = "Global"
+  }
 }
